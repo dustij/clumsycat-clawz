2410381bcd18c87b58b0566e73723249eddbed17
dustij <knightlybyte@gmail.com>
2024-03-11 18:27:29 -0600

    Mobile nav bar, menu button animates

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,66 @@
+"use client"
+
+import { motion } from "framer-motion"
 import Image from "next/image"
 import Link from "next/link"
+import { useEffect, useState } from "react"
+
+import { cn } from "@/lib/utils"
 
 import MobileNav from "./MobileNav"
 import Nav from "./Nav"
 
+const topVariants = {
+  closed: {
+    rotate: 0,
+  },
+  opened: {
+    rotate: 45,
+  },
+}
+
+const centerVariants = {
+  closed: {
+    opacity: 1,
+  },
+  opened: {
+    opacity: 0,
+    width: 0,
+  },
+}
+
+const bottomVariants = {
+  closed: {
+    rotate: 0,
+  },
+  opened: {
+    rotate: -45,
+  },
+}
+
 const Header = () => {
+  const [headerActive, setHeaderActive] = useState(false)
+  const [openNav, setOpenNav] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // detect scroll
+      setHeaderActive(window.scrollY > 50)
+    }
+
+    // add scroll event
+    window.addEventListener("scroll", handleScroll)
+    // clear scroll event
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
-    <header className="bg-background_light fixed left-0 right-0 h-[70px] w-full">
+    <header
+      className={cn(
+        "fixed top-0 z-50 h-[70px] w-full bg-background_light",
+        headerActive && "shadow-md",
+      )}
+    >
       <div className="container mx-auto flex h-full items-center justify-between">
         <Link href="/">
           <div className="flex items-center gap-1">
@@ -16,8 +70,38 @@ const Header = () => {
             </h1>
           </div>
         </Link>
-        <MobileNav containerStyles="xl:hidden" />
+        <MobileNav
+          containerStyles={cn(
+            "xl:hidden transition-all flex-col text-center gap-8 flex fixed bg-background_light w-full left-0 top-[70px] text-base uppercase font-medium",
+            openNav
+              ? "max-h-max pt-8 pb-10 border-t border-surface_variant_light"
+              : "max-h-0 pt-0 pb-0 overflow-hidden border-surface_variant_light/0",
+          )}
+        />
         <Nav containerStyles="bg-background flex gap-4 hidden xl:flex" />
+        {/* hide/show menu button */}
+        <div>
+          <button
+            className="flex h-8 w-10 flex-col items-center justify-between xl:hidden"
+            onClick={() => setOpenNav(!openNav)}
+          >
+            <motion.div
+              variants={topVariants}
+              animate={openNav ? "opened" : "closed"}
+              className="h-1 w-10 origin-left rounded-full bg-primary_light"
+            />
+            <motion.div
+              variants={centerVariants}
+              animate={openNav ? "opened" : "closed"}
+              className="h-1 w-10 origin-center rounded-full bg-primary_light"
+            />
+            <motion.div
+              variants={bottomVariants}
+              animate={openNav ? "opened" : "closed"}
+              className="h-1 w-10 origin-left rounded-full bg-primary_light"
+            />
+          </button>
+        </div>
       </div>
     </header>
   )
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,7 +1,37 @@
+"use client"
+
+import { Link } from "react-scroll"
+
 import { cn } from "@/lib/utils"
 
+const links = [
+  { name: "home", target: "home", offset: -100 },
+  { name: "about", target: "about", offset: -90 },
+  { name: "how to buy", target: "how-to-buy", offset: -90 },
+  { name: "tokenomics", target: "tokenomics", offset: -90 },
+  { name: "roadmap", target: "roadmap", offset: -90 },
+]
+
 const MobileNav = ({ containerStyles }: { containerStyles: string }) => {
-  return <nav className={cn(containerStyles)}>MobileNav</nav>
+  return (
+    <nav className={cn(containerStyles)}>
+      {links.map((link, index) => {
+        return (
+          <Link
+            key={index}
+            offset={link.offset}
+            to={link.target}
+            smooth
+            spy
+            activeClass="active"
+            className="cursor-pointer transition-all duration-300 hover:text-primary_light/90"
+          >
+            {link.name}
+          </Link>
+        )
+      })}
+    </nav>
+  )
 }
 
 export default MobileNav
