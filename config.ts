835ea01aecc3ec5f95beabaac7269fe2c49d622c
dustij <knightlybyte@gmail.com>
2024-03-21 05:44:31 -0600

    Updated links

--- config.ts
+++ config.ts
@@ -4,6 +4,8 @@ export const config = {
   telegram: "https://t.me/clumsycat_sol",
   solscan:
     "https://solscan.io/token/HsNsfNfngXfiWkrXReGRbbizMBg2NrMqX1KJyGddNkaJ",
-  raydium: "coming-soon",
-  dexscreener: "coming-soon",
+  raydium:
+    "https://raydium.io/swap/?inputCurrency=sol&outputCurrency=HsNsfNfngXfiWkrXReGRbbizMBg2NrMqX1KJyGddNkaJ&fixed=in",
+  dexscreener:
+    "https://dexscreener.com/solana/bm8uubswgevbtuf8ds33om9ychnx5kjckfr9huk6fbza",
 }
